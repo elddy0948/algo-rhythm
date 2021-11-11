908344b318dba52d6b301fb7dee46572931f423f
Joons <elddy0948@gmail.com>
2021-11-12 00:30:42 +0900

    11 / 12

---
+++ HackerRank/Compare the Triplets/Compare the Triplets/main.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+func compareTriplets(a: [Int], b: [Int]) -> [Int] {
+    // Write your code here
+  var result = [0, 0]
+  
+  for i in 0 ..< 3 {
+    if a[i] > b[i] {
+      result[0] += 1
+    } else if a[i] < b[i] {
+      result[1] += 1
+    }
+  }
+  
+  return result
+}
+
+guard let aTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let a: [Int] = aTemp.split(separator: " ").map {
+    if let aItem = Int($0) {
+        return aItem
+    } else { fatalError("Bad input") }
+}
+
+guard a.count == 3 else { fatalError("Bad input") }
+
+guard let bTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let b: [Int] = bTemp.split(separator: " ").map {
+    if let bItem = Int($0) {
+        return bItem
+    } else { fatalError("Bad input") }
+}
+
+guard b.count == 3 else { fatalError("Bad input") }
+
+let result = compareTriplets(a: a, b: b)
+
+print(result)
