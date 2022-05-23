8716382d6ded906d4c319043992c3969cd97b5d9
Joons <elddy0948@gmail.com>
2022-05-23 23:20:12 +0900

    프로그래머스 섬 연결하기

--- Programmers/ProgrammersSolutions/ProgrammersSolutions/main.swift
+++ Programmers/ProgrammersSolutions/ProgrammersSolutions/main.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-let problem = MakeBigNumber()
+let problem = ConnectIslands()
 
-print(problem.solution("1924", 2))
+print(problem.solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
