4a13e003391d97fbf5ad8fb1a5bfc85abc2f51ba
Joons <elddy0948@gmail.com>
2022-05-25 14:05:47 +0900

    프로그래머스 단어변환

--- Programmers/ProgrammersSolutions/ProgrammersSolutions/main.swift
+++ Programmers/ProgrammersSolutions/ProgrammersSolutions/main.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-let problem = ConnectIslands()
+let problem = ChangeWord()
 
-print(problem.solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
+print(problem.solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]  ))
