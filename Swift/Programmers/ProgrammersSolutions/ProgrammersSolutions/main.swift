4dd3962f11170d760a854e066cc30641362e2ece
Joons <elddy0948@gmail.com>
2022-08-02 14:52:40 +0900

    Programmers lv2 짝지어 제거하기
    removeLast() 보다 popLast()가 더 빠름..

--- Swift/Programmers/ProgrammersSolutions/ProgrammersSolutions/main.swift
+++ Swift/Programmers/ProgrammersSolutions/ProgrammersSolutions/main.swift
@@ -1,19 +1,5 @@
 import Foundation
 
-let problem = StringCompression()
-
-print(problem.solution("abcabcabcabcdededededede"))
-
-
-//func substring(string: String, range: Range<Int>) -> String {
-//  let startIndex = string.index(string.startIndex, offsetBy: range.lowerBound)
-//  let endIndex = string.index(string.startIndex, offsetBy: range.upperBound)
-//  let range = startIndex ..< endIndex
-//  return String(string[range])
-//}
-
-//print(substring(string: "abcd", range: 0 ..< 1))
-//
-//for i in stride(from: 0, to: 5, by: 1) {
-//  print(i)
-//}
+let problem = PairOff()
+let answer = problem.solution("cdcd")
+print(answer)
