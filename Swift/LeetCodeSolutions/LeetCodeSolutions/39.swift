469627c2fc7c5454a3aeb5b4b8f273ce9d1bae13
Joons <elddy0948@gmail.com>
2022-11-25 12:36:32 +0900

    39.Combination Sum

--- Swift/LeetCodeSolutions/LeetCodeSolutions/39.swift
+++ Swift/LeetCodeSolutions/LeetCodeSolutions/39.swift
@@ -1,7 +1,32 @@
 import Foundation
 
 class Solution39 {
+  func BackTracking(
+    _ candidates: [Int],
+    _ target: Int,
+    _ index: Int,
+    _ combinations: inout [[Int]],
+    _ combination: inout [Int]) {
+      
+      if target == 0 {
+        combinations.append(combination)
+        return
+      }
+      
+      for i in index ..< candidates.count {
+        guard candidates[i] <= target else { break }
+        combination.append(candidates[i])
+        BackTracking(candidates, target - candidates[i], i, &combinations, &combination)
+        combination.removeLast()
+      }
+    }
+  
   func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
-      return [[]]
+    var combinations = [[Int]]()
+    var combination = [Int]()
+    
+    BackTracking(candidates.sorted(), target, 0, &combinations, &combination)
+
+    return combinations
   }
 }
--- Swift/LeetCodeSolutions/LeetCodeSolutions/main.swift
+++ Swift/LeetCodeSolutions/LeetCodeSolutions/main.swift
@@ -1,1 +1,8 @@
 import Foundation
+
+
+let sol = Solution39()
+
+print(sol.combinationSum([2, 3, 6, 7], 7))
+
+
