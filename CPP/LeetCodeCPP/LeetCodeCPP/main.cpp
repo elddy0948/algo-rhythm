145a76d14c14622d9e6170ee7e7fa17e5ad05e51
Joons <elddy0948@gmail.com>
2022-12-29 13:16:07 +0900

    init leetcode cpp

---
+++ CPP/LeetCodeCPP/LeetCodeCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCodeCPP
+//
+//  Created by 김호준 on 2022/12/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
