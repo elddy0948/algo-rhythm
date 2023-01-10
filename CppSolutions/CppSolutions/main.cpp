a7f9f983fd453cdbba2f1898c21a75a057040728
Joons <elddy0948@gmail.com>
2023-01-10 15:33:40 +0900

    clean up folders

---
+++ CppSolutions/CppSolutions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPPSolutions
+//
+//  Created by 김호준 on 2023/01/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
