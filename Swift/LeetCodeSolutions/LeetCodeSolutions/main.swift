ae6486f7d83a442a218b25ac21ae96d1bf9318cf
Joons <elddy0948@gmail.com>
2023-01-10 16:03:00 +0900

    leetcode 216 Swift

--- Swift/LeetCodeSolutions/LeetCodeSolutions/main.swift
+++ Swift/LeetCodeSolutions/LeetCodeSolutions/main.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 
-let sol = Solution40()
+let sol = Solution216()
 
 var inp = [10,1,2,7,6,1,5]
 
-print(sol.combinationSum2([10,1,2,7,6,1,5], 8))
+print(sol.combinationSum3(4, 1))
 
 
